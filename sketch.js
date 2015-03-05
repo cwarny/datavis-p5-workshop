84dc5da8be1e05b952d46beea0ea0354059d3ecc
Cedric Warny <cedric.warny@gmail.com>
2015-03-05 18:39:04 -0500

    simplified data loading

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var radius = 250;
 
 function setup() {
 	createCanvas(displayWidth, displayHeight);
-	setupFriends();
+	loadJSON("data.json", setupFriends);
 };
 
 function draw() {
@@ -13,22 +13,18 @@ function draw() {
 	}
 };
 
-function setupFriends() {
-	var jsn = loadJSON("data.json", function(data) {
-		var data = jsn.friends.data;
-
-		for (var i=0; i<data.length; i++) {
-			var name = data[i].name;
-			var id = data[i].id;
-			var mutualfriends = [];
-			if (data[i].hasOwnProperty("mutualfriends")) {
-				mutualfriends = data[i].mutualfriends.data;
-			}
-			var theta = map(i, 0, data.length, 0, TWO_PI);
-			var x = width / 2 + cos(theta) * radius;
-			var y = height / 2 + sin(theta) * radius;
-			var f = new Friend(name, id, mutualfriends, x, y, theta);
-			allfriends[id] = f;
+function setupFriends(data) {
+	for (var i=0; i<data.length; i++) {
+		var name = data[i].name;
+		var id = data[i].id;
+		var mutualfriends = [];
+		if (data[i].hasOwnProperty("mutualfriends")) {
+			mutualfriends = data[i].mutualfriends.data;
 		}
-	});
+		var theta = map(i, 0, data.length, 0, TWO_PI);
+		var x = width / 2 + cos(theta) * radius;
+		var y = height / 2 + sin(theta) * radius;
+		var f = new Friend(name, id, mutualfriends, x, y, theta);
+		allfriends[id] = f;
+	}
 };
